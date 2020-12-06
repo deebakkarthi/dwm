e59312b49c84fd4f456b85079dfdc716a8c785b7
Deebakkarthi C R <deebakkarthicr@protonmail.com>
2020-12-06 18:44:59 +0530

    Vanity Gaps patch

--- config.h
+++ config.h
@@ -2,21 +2,33 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+
 static const int swallowfloating    = 1;       /* 1 means swallow floating windows by default*/
+
 static const int showbar            = 1;        /* 0 means no bar */
+
 static const int showsystray        = 1;       /*0 means no tray*/
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+
 static const int topbar             = 1;        /* 0 means bottom bar */
+
 static const char *fonts[]          = {"Monaco:size=13:style=Regular", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Monaco:size=13";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char sel_border[]      = "#770000";
+static const char sel_border[]      = "#007aff";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -42,7 +54,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -138,11 +150,11 @@ static Key keys[] = {
     /*{ MODKEY,                       XK_p,      spawn,           SHCMD("") }, */ 
     /*{ MODKEY|ShiftMask,             XK_p,      spawn,           SHCMD("") }, */
 
-    /*{ MODKEY,                       XK_bracketleft,     spawn,      SHCMD("") },*/
-    /*{ MODKEY|ShiftMask,             XK_bracketleft,     spawn,      SHCMD("") },*/
+    { MODKEY,                       XK_bracketleft,     incrgaps,     {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_bracketleft,     incrgaps,     {.i = -1 } },
 
-    /*{ MODKEY,                       XK_bracketright,    spawn,      SHCMD("") },*/
-    /*{ MODKEY|ShiftMask,             XK_bracketright,    spawn,      SHCMD("") },*/
+    { MODKEY,                       XK_bracketright,    defaultgaps,  {0} },      
+    { MODKEY|ShiftMask,             XK_bracketright,    togglegaps,   {0} },   
 
     /*{ MODKEY,                       XK_backslash,       spawn,      SHCMD("") },*/
     /*{ MODKEY|ShiftMask,             XK_backslash,       spawn,      SHCMD("") }, */
